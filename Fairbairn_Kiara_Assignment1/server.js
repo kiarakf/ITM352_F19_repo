5a87c76e510bfbf318e705dedc1574be145e1d27
Kiara Fairbairn <54615024+kiarakf@users.noreply.github.com>
2019-11-13 22:22:55 -1000

    Update server.js

--- Fairbairn_Kiara_Assignment1/server.js
+++ Fairbairn_Kiara_Assignment1/server.js
@@ -1,68 +1,51 @@
-var express = require('express');
-var app = express();
-var myParser = require("body-parser");
-var products = require ("./public/product_data.js");
-const querystring = require('querystring');
-
+//from Lab 13 and Ports Office Hours
+var express = require('express'); //requires the express package in the node_modules
+var app = express(); //starts express
+var myParser = require("body-parser"); //need to use bodyParser() to form data to be available in req.body
+var products = require ("./public/product_data.js"); //defines the products variable and gets data from the product_data.js array
+const querystring = require('querystring'); //gives utilities for parsing and formatting URL query strings
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
 
+//from Lab 13 and Ports Office Hours
 app.use(myParser.urlencoded({ extended: true }));
 app.post("/process_form", function (request, response) {
-    let POST = request.body;
-    var hasValidQuantities = true;
-    var hasPurchases = false;
-    for (i = 0; i < products.length; i++) {
-        q = POST ['quantity' +i];
-        if (isNonNegInt(q) == false){
-            hasValidQuantities = false;
+    let POST = request.body; //
+    var hasValidQuantities = true; //defining the hasValidQuantities variable and assuming all quantities are valid
+    var hasPurchases = false; //assume the quantity of purchases are false
+    for (i = 0; i < products.length; i++) { //for loop for each product array that increases the count by 1
+        q = POST ['quantity' +i]; //quantity entered by the user for a product is aessigned into q
+        if (isNonNegInt(q) == false){ //if the quantity enetered by the user is invalid integer
+            hasValidQuantities = false; //hasValidQuantities is false or nothing was inputed in the quantity textbox
         }
-        if (q>0) {
-            hasPurchases = true;
+        if (q>0) { //if quantity entered in the textbox is greater than 0
+            hasPurchases = true; //if q is greater than 0 than the hasPurchases is ok
         }
     }
     //if data is valid give user an invoice, if not give them an error
-     qString = querystring.stringify (POST);
-     if (hasValidQuantities == true && hasPurchases == true) {
-       
-        response.redirect ("./invoice.html?" + qString );
+     qString = querystring.stringify (POST); //string query together
+     if (hasValidQuantities == true && hasPurchases == true) { //if both hasValidQuantities variable and hasPurchases variable are valid 
+        response.redirect ("./invoice.html?" + qString ); //if quantity is valid it will send user to invoice
     }
     else {
-        response.redirect ("./products_display.html?" + qString); 
-    }
-
-  
-    //cut and pasted from order_page copy.htmlf
-    if (typeof POST['quantity_textbox'] != 'undefined') {
-        displayPurchase(POST, response);
-
+        response.redirect ("./products_display.html?" + qString); //if quantity is invalid it will send user back to products page
     }
 });
 
-app.use(express.static('./public'));
-app.listen(8080, () => console.log(`listening on port 8080`));
+
+//taken from Lab 13
+app.use(express.static('./public'));  //takes get request and look for file in public directly
+app.listen(8080, () => console.log(`listening on port 8080`)); //makes the server listen on Port 8080 and print smessage into console
 
 //copied from order_page
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
-    if (q== '') {q=0};
+    if (q== '') {q=0}; //if quantity is empty or 0
     if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
     if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    return returnErrors ? errors : (errors.length == 0);
-
-}
-// cut and pasted from order_page copy
-function displayPurchase(POST, response) {
-
-    // Exercise 2b. Does not rewrite page if input is a NonNegInt
-    if (isNonNegInt(POST['quantity_textbox'])) {
-        response.send(`Thank you for purchasing ${q} things!`); 
-    }
-    else {
-        response.send(`${q} is not a quantity! Press the back button and try again.`); 
-    }
+    return returnErrors ? errors : (errors.length == 0); //returns error if any
 }
