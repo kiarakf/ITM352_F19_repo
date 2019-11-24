30b1259def6b7c0181289f61c90327b3ba5634d3
Kiara Fairbairn <54615024+kiarakf@users.noreply.github.com>
2019-11-24 13:35:00 -1000

    Add files via upload

--- Fairbairn_Kiara_Assignment2/server.js
+++ Fairbairn_Kiara_Assignment2/server.js
@@ -85,88 +85,106 @@ if (fs.statSync(filename)) {
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
     console.log(request.body);
-    the_username = request.body.username; //give me username from object and assigning it
+    var qString = querystring.stringify(request.query);
+    inputUser= request.body.username;
+    inputPass = request.body.password;
+    the_username = request.body.username.toLowerCase(); //give me username from object and assigning it
+    
+    
     if (typeof users_reg_data[the_username] != 'undefined') { //ask object if it has property called username, if it does, it wont be udefined. check to see if it exists
         if (users_reg_data[the_username].password == request.body.password) {//check if the password they entered matches what was stored
             //passes the username + the string logged in on the page to greet them
+            
+            presentFullName= users_reg_data[the_username].name;
+            stickPut = presentFullName;
+            request.query.stickMe2 = stickPut;
+            qString = querystring.stringify(request.query);
             response.redirect("./invoice.html?" + qString);//if the quantity is valid, user is directed to invoice along with the query data from the form
         } else if (users_reg_data[the_username].password != request.body.password) {
-            response.send('password incorrect');//if they did not login successfully, does another get request and redirects user to login to page
+            error = "Incorrect Password";
+            stickInput = inputUser;
+            //if they did not login successfully, does another get request and redirects user to login to page
             //can regnerate form here and display errors
         }
-    } else if (the_username.length == 0 || request.body.password == 0) {
-        response.send('please enter a username and password');
     } else {
-        response.redirect('/register?' + qString);
+        error = the_username + " is not registered";
+        stickInput = inputUser
     }
-
+request.query.LoginError = error;
+request.query.stickies = stickInput;
+qString = querystring.stringify(request.query);
+response.redirect("./login_page.html?" + qString);
 });
 
-app.get("/register", function (request, response) { //if server gets request to register
-    // Give a simple register form
-    //when submit, posts to register, then calls app.post
-    str = `
-        <body>
-        <form action="" method="POST">
-        <input type="text" name="full_name" size="40" placeholder="enter full name"> 
-        <label id="fullNamelabel" class=""> Full Name </label> <br/>
-        <input type="text" name="username" size="40" placeholder="enter username"> 
-        <label id="usernameLabel" class=""> username </label> <br/>
-        <input type="password" name="password" size="40" placeholder="enter password">
-        <label id="passwordLabel" class=""> password </label> <br/>
-        <input type="password" name="repeat_password" size="40" placeholder="enter password again">
-        <label id="repeatpasswordLabel" class=""> Repeat Password </label> <br/>
-        <input type="email" name="email" size="40" placeholder="enter email">
-        <label id="emaillabel" class=""> Email </label> <br/>
-        <input type="submit" value="Submit" id="submit">
-        </form>
-        </body>
-    `;
-    response.send(str);
-});
+
 
 app.post("/register", function (request, response) {
     // process a simple register form
 
     //validate registration data
-
     //all good, so save new user to the file name(registration data)
     username = 'newuser';
     username = request.body.username.toLowerCase(); //makes the username insensitive
-    full_name = request.body.full_name();
-    email = request.body.email.toLowerCase();
+    fullname = request.body.fullname;
+
+
+    var qString = querystring.stringify(request.query);
+    regInputUser = request.body.username;
+    regInputPass = request.body.password;
+    regFullName = request.body.fullname;
+    regPassword = request.body.password;
+    regRepPassword = request.body.repeat_password;
+    regEmail = request.body.email;
+    email =request.body.email.toLowerCase();
     if (typeof users_reg_data[username] != 'undefined') {//check if the password they entered matches what was stored
-        response.redirect('/login_page.html?' + qString);//if they did not login successfully, does another get request and redirects user to login to page
+        //response.redirect('./login.html?' + qString);//if they did not login successfully, does another get request and redirects user to login to page
         //can regnerate form here and display errors
-    } else if (request.body.full_name > 30) {
-        response.send ('your username is too long')
-    } else if (!(/^[a-zA-Z]+$/.test(fullname))) {
-        response.send('username must only be characters')
-    } else if (request.body.password.length <6 ){
-        response.send ('password must be a minimum of 6 characters')
+    } else if (request.body.fullname.length > 30){
+        //response.send('your name is too damn long, shorten it');
+        errors2='fullname is too long';
+        stickType = regFullName;
+    } else if (!(/^[A-Za-z ]+$/.test(fullname))){
+        errors2='name must be letters only';
+        stickType = regFullName;
+    } else if (request.body.password.length < 6){
+        errors2='password must be more than 6 characters long';
     } else if (request.body.repeat_password != request.body.password) {
-        response.send(`password does not match`);
-    } else if (!(/^[a-zA-Z0-9]+$/.test(username))) { //code from my sister-in-law
-        response.send('username can only consist of characters and numbers');
-    } else if (username.length < 4){
-        response.send('username must be a minimum of 4 characters');
-    } else if (username.length > 10){
-        response.send('username can not be more than 10 characters');
-    } else if (!(/^[a-zA-Z0-9._]+@[a-zA-Z.]+\.[a-zA-Z]{2,3}$/.test(email))) { //code from sister in law. [a-zA-Z0-9._]+ this means letters or numbers or “.” or “_” + theres an @. [a-zA-Z.]+ this means letters or numbers or ".". \. this means “.”. a-zA-Z]{2,3} this means 2 or 3 letters.
-        response.send ('email must meet the requirements. <br> The requirements are: <br> 1. email must be  in an X@Y.Z format where X is the user address which can only contain letters, numbers, and the characters “_” and “.” <br> 2. Y is the host machine which can contain only letters and numbers and “.” characters <br>3. Z is the domain name which is either 2 or 3 letters such as “edu” or “tv”.')
-    } else {
+        errors2='password does not match';
+    } else if (!(/^[a-zA-Z0-9]+$/.test(username))) { //kiara's siter in law
+        errors2='useusername must be characters and numbers only';
+        stickType = regInputUser;
+    } else if (username.length > 10 ){
+        errors2='username is too long';
+        stickType = regInputUser
+    } else if (username.length < 4) {
+        errors2='username is too short';
+        stickType = regInputUser;
+    } else if (!(/^[a-zA-Z0-9._]+@[a-zA-Z.]+\.[a-zA-Z]{2,3}$/.test(email))){
+        errors2='email is invalid';
+        stickType = regEmail;
+    }
+    else {   
         users_reg_data[username] = {}; //new user becomes new property of users_reg_data object
-        users_reg_data[username].name = request.body.name;
+        users_reg_data[username].name = request.body.fullname;
         users_reg_data[username].password = request.body.password;
         users_reg_data[username].email = request.body.email;
         fs.writeFileSync(filename, JSON.stringify(users_reg_data));
         //alert(`${username} registered!`)
-        response.redirect("/invoice.html?" + qString);
+        
+        stickType2 = regFullName;
+        request.query.stickMe2 = stickType2; 
+        qString = querystring.stringify(request.query);
+        response.redirect("./invoice.html?" + qString);
+    
         //response.redirect ("./invoice.html?" + qString );//if the quantity is valid, user is directed to invoice along with the query data from the form
+
         console.log(request.body);
-    }
+}   
+    request.query.RegisterError = errors2;
+    request.query.stickMe = stickType;
+    qString = querystring.stringify(request.query);
+    response.redirect("./registration.html?" + qString);    
 
 });
-
 
 
